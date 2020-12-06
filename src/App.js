bc344338ef1593b5b39f288284d78e0455e9673f
arif93sulaiman <arifhip1993@yahoo.com>
2020-12-06 09:44:28 +0800

    changes 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from "./moviesList";
 
 function App() {
+  const [movies, setMovies] = useState([])
+
+  const getMovieList = async () => {
+    const url = "http://www.omdbapi.com/?s=star wars&apikey=69ccef59"
+    const response = await fetch(url)
+    const resJson = await response.json()
+    const listMovies = await resJson.Search
+
+    setMovies(listMovies)
+  }  
+
+  useEffect(() => {
+    getMovieList()     
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MovieList movies={movies}/>   
     </div>
   );
 }
